875ec05a1eeb4ddbf8966bd4d8949e45a2c6e476
Matheus Mazoni <mmazoni.andrade@gmail.com>
2024-11-10 12:29:47 -0300

    update config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,24 @@
 # --------------PATHS-----------------------
 
 # set -x PATH /home/mmazoniandrade/.local/bin/ $PATH
-# set -x PATH /home/mmazoniandrade/miniconda3/bin/ $PATH
+set -x PATH "$PATH":"$HOME/.local/scripts/"
 
 set pipenv_fish_fancy yes
 
-if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+# if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 # eval (python3 -m virtualfish compat_aliases auto_activation global_requirements)
 
 # default shell : sudo chsh -s /usr/bin/fish mmazoniandrade
 
-fundle plugin 'tuvistavie/fish-theme-afowler'
+if not type -q omf; eval (curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
+    fish install --path=~/.local/share/omf --config=~/.config/omf); end
 
-fundle init
+if not omf theme sashimi; omf install sashimi; omf theme sashimi; end
+
+if type -q kubectl; eval (kubectl completion fish > ~/.config/fish/completions/kubectl.fish); end
+
+bind \cf "tmux-sessionizer"
 
 # source /home/mmazoniandrade/miniconda3/etc/fish/conf.d/conda.fish
 
